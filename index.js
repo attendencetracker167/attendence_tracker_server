10ca387790d5338b06721dc7702c2fbd38e9381e
attendencetracker167 <attendencetracker167@gmail.com>
2024-08-15 03:05:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ require("dotenv").config();
 const cors = require("cors");
 const upload = require("express-fileupload");
 const { notFound, erorMiddleware } = require("./Middleware/errorMiddleware");
+
+const port = process.env.PORT || 5000;
 app.use(upload());
 app.use('/uploads', express.static(__dirname + '/uploads'))
 app.use(express.json({ limit: '70mb', extended: true }));
@@ -32,7 +34,7 @@ app.use(notFound)
 app.use(erorMiddleware)
 
 connect(process.env.MONGO_DB_URL).then(() => {
-app.listen(process.env.PORT, () => {
+app.listen( port , '0.0.0.0' , () => {
   console.log(
     `DB CONNECTED SUCCESSFULLY AND SERVER RUNNING ON ${process.env.PORT}`,
     
